dae8d7f26486c171bddd24eea790a5c107f1aa09
EARNascimento <eduardo90909@outlook.com>
2025-08-17 15:25:13 -0300

    Criando a classe TopicDataReceiverDTO

---
+++ src/main/java/br/com/alura/forumhub/domain/topico/TopicDataReceiverDTO.java
@@ -0,0 +1,23 @@
+package br.com.alura.forumhub.domain.topico;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record TopicDataReceiverDTO(
+        @JsonAlias("titulo")
+        @NotBlank
+        String title,
+        @JsonAlias("mensagem")
+        @NotBlank
+        String message,
+        @JsonAlias("data_criacao")
+        @NotNull
+        @FutureOrPresent
+        LocalDateTime creationDate
+) {
+
+}
